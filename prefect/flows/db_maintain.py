c96c9c2d3c1c29bce357ca18bd9e8b2ea6528a4d
kevin000001505 <a10121555@gmail.com>
2026-03-01 18:31:15 +0000

    Turn the print into logger

--- prefect/flows/db_maintain.py
+++ prefect/flows/db_maintain.py
@@ -1,38 +1,42 @@
 from database.db_tools import get_connection
+from prefect import flow, get_run_logger
 from tasks.db import (
     initial_drap_db,
     initial_activate_flight_db,
     initial_airport_db,
     cleanup_old_drap_data,
     initial_latest_xray_db,
+    initial_proton_flux_plot_db,
 )
-from prefect import flow
 
 
 @flow(log_prints=True)
 async def db_maintenance_flow():
     """Flow to perform database maintenance tasks."""
-    print("Starting database maintenance flow...")
+    logger = get_run_logger()
+    logger.info("Starting database maintenance flow...")
     try:
         async with get_connection() as conn:
             await cleanup_old_drap_data(conn, older_than_days=1)
-            print("Database maintenance completed successfully!")
+            logger.info("Database maintenance completed successfully!")
 
     except Exception as e:
-        print(f"Database maintenance failed: {e}")
+        logger.error(f"Database maintenance failed: {e}")
 
 
 @flow(log_prints=True)
 async def initialize_db_flow():
     """Flow to initialize the database schema."""
-    print("Starting database initialization flow...")
+    logger = get_run_logger()
+    logger.info("Starting database initialization flow...")
     try:
         async with get_connection() as conn:
             await initial_drap_db(conn)
             await initial_airport_db(conn)
             await initial_activate_flight_db(conn)
             await initial_latest_xray_db(conn)
-            print("Database initialization completed successfully!")
+            await initial_proton_flux_plot_db(conn)
+            logger.info("Database initialization completed successfully!")
 
     except Exception as e:
-        print(f"Database initialization failed: {e}")
+        logger.error(f"Database initialization failed: {e}")
