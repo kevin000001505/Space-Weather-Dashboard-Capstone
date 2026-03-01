f5232631dccbb7be202b5af5816bc6cbc3abc6b4
kevin000001505 <a10121555@gmail.com>
2026-03-01 18:30:58 +0000

    Fix redundant conn input into function

--- prefect/flows/airports_extract.py
+++ prefect/flows/airports_extract.py
@@ -11,10 +11,7 @@ async def airports_extract_flow():
     logger = get_run_logger()
 
     try:
-        async with get_connection() as conn:
-            await airports.ingest_airports_csv(conn)
-            logger.info(
-                "Airports data extraction and ingestion completed successfully!"
-            )
+        await airports.ingest_airports_csv()
+        logger.info("Airports data extraction and ingestion completed successfully!")
     except Exception as e:
         logger.error(f"Airports data extraction failed: {e}")
