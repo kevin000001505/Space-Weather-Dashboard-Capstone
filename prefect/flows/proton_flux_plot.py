22ed29b829ef55ee0d8f5da35908637cbe1de715
kevin000001505 <a10121555@gmail.com>
2026-03-02 04:46:24 +0000

    Add retry into Proton Flux

--- prefect/flows/proton_flux_plot.py
+++ prefect/flows/proton_flux_plot.py
@@ -7,7 +7,12 @@ from tasks.proton_flux_plot import (
 )
 
 
-@flow(name="Ingest Proton Flux Plot Data", log_prints=True)
+@flow(
+    name="Ingest Proton Flux Plot Data",
+    log_prints=True,
+    retries=3,
+    retry_delay_seconds=5,
+)
 async def ingest_proton_flux_plot_flow():
     """Main flow for ingesting proton flux plot data from NOAA."""
     records = fetch_proton_flux_plot()
